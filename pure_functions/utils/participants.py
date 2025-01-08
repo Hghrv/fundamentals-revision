1ca7e5c6eff031923174cfec7569a2514cc510cd
hghrv <htchoumkeu@yahoo.fr>
2025-01-08 23:04:18 +0000

    complete the Pure Functions tasks

--- pure_functions/utils/participants.py
+++ pure_functions/utils/participants.py
@@ -1,4 +1,9 @@
 def participants(goats):
-    for goat in goats:
-        del goat['time_in_seconds']
-    return goats
+    #copy the argument to ensure purity
+    goats_copy = [goat for goat in goats]
+    #initialising the output
+    output =[]
+    #append name dictionary to output and return output
+    for goat in goats_copy:
+        output.append({key: goat[key] for key in goat if key == 'name'})
+    return output
